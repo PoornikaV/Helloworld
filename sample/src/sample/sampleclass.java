01a656958b7823d508db90505ea3f2eae6abf30c
PoornikaV <Poornika.v@cognizant.com>
2017-09-14 02:43:04 -0400

    Add files via upload

---
+++ sample/src/sample/sampleclass.java
@@ -0,0 +1,34 @@
+package sample;
+
+/*import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;*/
+/*import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;*/
+
+
+
+public class sampleclass {
+	
+
+	public static void main(String[] args) {
+	/*	System.setProperty("webdriver.chrome.driver","C:\\Users\\bpstestr\\Desktop\\chromedriver.exe");
+		String baseurl="https://mail.cognizant.com";
+		WebDriver driver=new ChromeDriver();
+		driver.get(baseurl);*/
+		System.out.println("Hello world");
+	}
+		
+
+
+}
